50bd43d6a2d1a6b6ae639e15abecbff880dfdc47
Seven <askdaddy@gmail.com>
2021-12-21 17:53:16 +0800

    WIP

--- public/client.js
+++ public/client.js
@@ -1,47 +1,153 @@
-let uuid;
 let serverConnection;
-let peerConnection;
+const peerConnections = {};
+let localStream;
+const peerConf = {
+    'iceServers': [
+        {'url': 'stun:stun.services.mozilla.com'},
+        {'urls': 'stun:stun.stunprotocol.org:3478'},
+        {'urls': 'stun:stun.l.google.com:19302'}
+    ]
+}
+const constraints = {
+    video: true,
+    audio: false
+}
 
 function domReady() {
-    uuid = createUUID();
-
-    serverConnection = new WebSocket('ws://' + window.location.hostname + ':12000');
+    serverConnection = new WebSocket('wss://' + window.location.hostname + ':8443');
     serverConnection.onmessage = messageHandler;
 
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        })
-            .then(getUserMediaSuccess)
-            .catch(errorHandler);
-    } else {
-        alert('Your browser does not support getUserMedia API');
+    serverConnection.onopen = function () {
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(constraints)
+                .then(getUserMediaSuccess)
+                .catch(console.error);
+        } else {
+            alert('Your browser does not support getUserMedia API');
+        }
     }
 }
 
 function messageHandler(message) {
+    if (!message.data || typeof message.data !== "string") return;
+    const signal = JSON.parse(message.data)
+        , uuid = signal.uuid;
 
-}
-
-function getUserMediaSuccess(stream) {
-    // localStream = stream;
-    // localVideo.srcObject = stream;
-    let local_media = $("<video>");
-    local_media.attr("autoplay", "autoplay");
-    local_media.attr("controls", "");
-    $('body').append(local_media);
-    local_media[0].srcObject = stream;
-}
-
-function createUUID() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    console.log(signal);
+    if (signal.join) {
+        console.log('on join >>',signal);
+        call(uuid);
+        return;
     }
 
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    const _pc = launchPeer(uuid);
+    if (!_pc) return;
+
+
+    if (signal.sdp) {
+        console.log(`sdp: `, signal);
+        _pc.setRemoteDescription(new RTCSessionDescription(signal.sdp))
+            .then(function () {
+                if (signal.sdp.type == 'offer') {
+                    _pc.createAnswer()
+                        .then(function (desc) {
+                            createdDescription(_pc, desc);
+                        })
+                        .catch(console.error);
+                }
+            })
+            .catch(console.error);
+    }
+
+    if (signal.ice) {
+        console.log(`ice: `, signal);
+        _pc.addIceCandidate(new RTCIceCandidate(signal.ice))
+            .catch(console.error);
+    }
+
+    if (signal.close) {
+        // TODO remove <video> object.
+        console.info(signal);
+        const close_tag = document.getElementById(signal.uuid);
+        if (close_tag) close_tag.remove();
+        peerConnections[signal.uuid] = undefined;
+    }
 }
 
-function errorHandler(error) {
-    console.log(error);
+
+function call(uuid) {
+    const tmpPeerConn = new RTCPeerConnection(peerConf);
+    peerConnections[uuid] = tmpPeerConn;
+    tmpPeerConn.onicecandidate = gotIceCandidate;
+    tmpPeerConn.ontrack = function (ev) {
+        gotRemoteStream(uuid, ev.streams[0]);
+    }
+    tmpPeerConn.addStream(localStream);
+
+    tmpPeerConn.createOffer()
+        .then(function (desc) {
+            createdDescription(tmpPeerConn, desc)
+        })
+        .catch(console.error);
+
 }
+
+function launchPeer(uuid) {
+    console.log(`launchPeer:`, uuid);
+    let _pc = peerConnections[uuid];
+    if (!_pc) {
+        _pc = new RTCPeerConnection(peerConf);
+        peerConnections[uuid] = _pc;
+        // return console.error(`empty peer`);
+    }
+    _pc.onicecandidate = gotIceCandidate;
+    _pc.ontrack = function (ev) {
+        gotRemoteStream(uuid, ev.streams[0]);
+    };
+    _pc.addStream(localStream);
+    return _pc;
+}
+
+function gotIceCandidate(ev) {
+    console.log(`got ice: `, ev.candidate);
+    if (ev.candidate !== null) {
+        serverConnection.send(JSON.stringify({'ice': ev.candidate}));
+    }
+}
+
+function gotRemoteStream(uuid, stream) {
+    console.log('got remote stream: ', uuid);
+    const others = document.getElementById("others");
+    const _media = document.createElement("video");
+    _media.setAttribute("autoplay", "autoplay");
+    _media.setAttribute("id", uuid);
+
+    others.append(_media);
+    _media.srcObject = stream;
+}
+
+function createdDescription(pc, description) {
+    if (!pc) return;
+    console.log('createdDescription');
+    pc.setLocalDescription(description).then(function () {
+        serverConnection.send(JSON.stringify({'sdp': pc.localDescription}));
+    }).catch(console.error);
+}
+
+
+function getUserMediaSuccess(stream) {
+    localStream = stream;
+    const main = document.getElementById("main")
+    let local_media = document.createElement("video");
+    local_media.setAttribute("id", "local");
+    local_media.setAttribute("muted", true);
+    local_media.setAttribute("autoplay", "autoplay");
+    local_media.setAttribute("controls", "");
+
+    local_media.srcObject = stream;
+    main.append(local_media);
+
+    serverConnection.send(JSON.stringify({"join": true}));
+}
+
+
--- server.js
+++ server.js
@@ -1,53 +1,68 @@
-const PORT = 12000
-
+const HTTPS_PORT = 8443
 const fs = require('fs');
-const http = require('http');
+const https = require('https');
 const WebSocket = require('ws');
 
-const requestListener = function (req, res) {
-    console.log('request received: ' + req.url);
+const serverConfig = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+};
+const handleRequest = function (request, response) {
+    // Render the single client html file for any request the HTTP server receives
+    console.log('request received: ' + request.url);
 
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(fs.readFileSync('public/index.html'));
-    } else if (req.url === '/client.js') {
-        res.writeHead(200, {'Content-Type': 'application/javascript'});
-        res.end(fs.readFileSync('public/client.js'));
+    if (request.url === '/') {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(fs.readFileSync('public/index.html'));
+    } else if (request.url === '/client.js') {
+        response.writeHead(200, {'Content-Type': 'application/javascript'});
+        response.end(fs.readFileSync('public/client.js'));
     }
-}
+};
 
-const httpServer = http.createServer(requestListener);
-httpServer.listen(PORT, '0.0.0.0');
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT, '0.0.0.0');
 
-const wsServer = new WebSocket.WebSocketServer({server: httpServer, clientTracking: true});
+const wsServer = new WebSocket.WebSocketServer({server: httpsServer, clientTracking: true});
 
-wsServer.on('connection', function (ws) {
-    ws.userData = "";
+wsServer.on('connection', function (client, req) {
+    const ip = req.socket.remoteAddress;
+    const port = req.socket.remotePort;
+    const id = createUUID();
+    client['remoteAddress'] = `${ip}:${port}`;
+    client['socket_id'] = id;
+    console.info(`Client connected[${client.socket_id}] from ${client.remoteAddress}`);
 
-    ws.on('message', function (data, isBinary) {
-        // Broadcast any received message to all clients
-        console.log(`received: ${data}`);
-        ws.userData = data;
-
-        wsServer.clients.forEach(function (client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data, {binary: isBinary});
-            }
-        });
+    client.on('message', function (data, isBinary) {
+        try {
+            const _do = JSON.parse(data);
+            _do.uuid = client.socket_id;
+            wsServer.broadcast(_do, client);
+        } catch (e) {
+            console.error(e);
+        }
     });
 
-    ws.on('close', function (code, reason) {
+    client.on('close', function (code, reason) {
         console.log(`connection close[${code}]: ${reason}`);
-
         // broadcasting to every other connected WebSocket clients, excluding itself.
-        wsServer.clients.forEach(function (client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                const tmp = JSON.parse(ws.userData);
-                tmp['close'] = true;
-                client.send(JSON.stringify(tmp));
-            }
-        });
+        wsServer.broadcast({"close": true, "uuid": client.socket_id}, client);
     });
 });
 
+wsServer.broadcast = function (messageObj, exclude) {
+    this.clients.forEach(function (client) {
+        if (client !== exclude && client.readyState === client.OPEN) {
+            client.send(JSON.stringify(messageObj));
+        }
+    });
+}
 
+
+function createUUID() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    return  s4() + '-' + s4() + '-' + s4() + s4() ;
+}
